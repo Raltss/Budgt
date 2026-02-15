232c04006b597a857c6e39c99c1f30ba617a0609
Manuel <barcelonamanuelmarc@gmail.com>
2026-02-15 12:42:03 +0800

    Try laravel pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'name', // asdadadad adadadada
         'email', // asdadadad adadadada
         'password', // asdadadad adadadada
     ];
