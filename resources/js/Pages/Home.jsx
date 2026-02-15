42f5dd07975ab7dae7b5d203a32eb921b0a846ab
Manuel <barcelonamanuelmarc@gmail.com>
2026-02-15 12:01:01 +0800

    Initial commit
    feat: install dependencies
    
    feat: configure files

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <button className="text-red-500 text-2xl">hello user! </button>
+        </div>
+    );
+}
+
+export default Home;
