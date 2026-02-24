d1698dfbfdb9c2bf685235b7f67764b88d92bb28
Manuel <barcelonamanuelmarc@gmail.com>
2026-02-24 10:09:39 +0800

    feat: add modal
    feat: intergrate modal to add button

---
+++ resources/js/Components/Modals.jsx
@@ -0,0 +1,36 @@
+import { forwardRef } from "react";
+
+const Modals = forwardRef((props, ref) => {
+    return (
+        <div>
+            <AddModal ref={ref} />
+        </div>
+    );
+});
+
+const AddModal = forwardRef((props, ref) => {
+    return (
+        <>
+            <dialog
+                id="my_modal_5"
+                className="modal modal-bottom sm:modal-middle"
+                ref={ref}
+            >
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">
+                        Press ESC key or click the button below to close
+                    </p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </>
+    );
+});
+
+export default Modals;
